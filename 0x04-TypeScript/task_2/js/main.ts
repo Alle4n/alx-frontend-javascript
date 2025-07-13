a34bf599e383a105cb836cde56b970e38b3b01f9
Alle4n <allennajjuma96@gmail.com>
2025-07-13 12:03:08 +0300

    7. String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -63,3 +63,16 @@ function executeWork(employee: Director | Teacher): string {
 console.log(executeWork(createEmployee(200)));     // Getting to work
 console.log(executeWork(createEmployee(400)));    // Getting to work
 console.log(executeWork(createEmployee(1000)));   // Getting to director tasks
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
+
+// Test output
+console.log(teachClass('Math'));     // Teaching Math
+console.log(teachClass('History'));  // Teaching History
